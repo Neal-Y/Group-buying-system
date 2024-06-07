b94f3e77a5f012e54a9920b671459df5f9b0aca7
neal <neal0420yang@gmail.com>
2024-06-07 10:33:21 +0800

    feature (line mvp use case) config setting

---
+++ config/config.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	LineClientID     string
+	LineClientSecret string
+	NgrokURL         string
+	LineRedirectURI  string
+}
+
+var AppConfig Config
+
+func LoadConfig() {
+	viper.SetConfigFile(".env")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatalf("Error while reading config file %s", err)
+	}
+
+	AppConfig = Config{
+		LineClientID:     viper.GetString("LINE_CLIENT_ID"),
+		LineClientSecret: viper.GetString("LINE_CLIENT_SECRET"),
+		NgrokURL:         viper.GetString("NGROK_URL"),
+		LineRedirectURI:  viper.GetString("NGROK_URL") + "/api/line/callback",
+	}
+}
