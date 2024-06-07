8b32a71ca92686153ec3302927db15a9dcf1aec0
neal <neal0420yang@gmail.com>
2024-06-07 10:33:45 +0800

    feature (line mvp constant) create constant

---
+++ constant/line.go
@@ -0,0 +1,7 @@
+package constant
+
+const (
+	LineAuthURL    = "https://access.line.me/oauth2/v2.1/authorize"
+	LineTokenURL   = "https://api.line.me/oauth2/v2.1/token"
+	LineProfileURL = "https://api.line.me/v2/profile"
+)
