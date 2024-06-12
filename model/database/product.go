59a76468a4cc36a13d0134d230a99a1a9691a35d
neal <neal0420yang@gmail.com>
2024-06-12 11:17:17 +0800

    feat(entity)products 資料庫CRUD

--- model/database/product.go
+++ model/database/product.go
@@ -7,15 +7,15 @@ import (
 )
 
 type Product struct {
-	ID             int       `gorm:"primary_key;autoIncrement"`
-	Name           string    `gorm:"type:varchar(255);not null"`
-	Picture        string    `gorm:"type:varchar(255)"`
-	Price          float64   `gorm:"type:decimal(10,2);not null"`
-	Stock          int       `gorm:"type:int;default:0"`
-	Description    string    `gorm:"type:text"`
-	ExpirationTime time.Time `gorm:"type:timestamp"`
-	CreatedAt      time.Time `gorm:"type:timestamp;default:CURRENT_TIMESTAMP"`
-	UpdatedAt      time.Time `gorm:"type:timestamp;default:CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"`
+	ID             int       `json:"id" gorm:"primary_key;autoIncrement"`
+	Name           string    `json:"name" gorm:"type:varchar(255);not null"`
+	Picture        string    `json:"picture" gorm:"type:varchar(255)"`
+	Price          float64   `json:"price" gorm:"type:decimal(10,2);not null"`
+	Stock          int       `json:"stock" gorm:"type:int;default:0"`
+	Description    string    `json:"description" gorm:"type:text"`
+	ExpirationTime time.Time `json:"expiration_time" gorm:"type:datetime"`
+	CreatedAt      time.Time `json:"created_at" gorm:"type:timestamp;default:CURRENT_TIMESTAMP"`
+	UpdatedAt      time.Time `json:"updated_at" gorm:"type:timestamp;default:CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"`
 }
 
 func (Product) TableName() string {
@@ -40,7 +40,7 @@ func (product *Product) Delete() error {
 	return product.model().Delete(product).Error
 }
 
-func (Product) FindAll() (products []Product, err error) {
+func FindAll() (products []Product, err error) {
 	err = infrastructure.Db.Find(&products).Error
 	return
 }
