cd82e87b8e5754f7fc85879e1be664965dfb2687
neal <neal0420yang@gmail.com>
2024-06-14 19:59:08 +0800

    feat(controller)create logic

--- handler/product/create.go
+++ handler/product/create.go
@@ -3,8 +3,6 @@ package product
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
-	"shopping-cart/infrastructure"
-	"shopping-cart/model/database"
 	"shopping-cart/model/datatransfer"
 )
 
@@ -17,26 +15,11 @@ func (h *Product) CreateProduct(c *gin.Context) {
 		return
 	}
 
-	productDao := database.Product{
-		Name:           productDto.Name,
-		Picture:        productDto.Picture,
-		Price:          productDto.Price,
-		Stock:          productDto.Stock,
-		Description:    productDto.Description,
-		ExpirationTime: productDto.ExpirationTime,
-	}
-
-	err = infrastructure.Db.Where("name = ?", productDto.Name).First(&productDao).Error
-	if err == nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Product name already exists"})
-		return
-	}
-
-	err = productDao.Create()
+	product, err := h.productService.CreateProduct(&productDto)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"message": "Product created successfully", "product": productDao})
+	c.JSON(http.StatusOK, gin.H{"message": "Product created successfully", "product": product})
 }
