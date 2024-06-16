1fff0ac18330e64b1b4f8652bfb6415d35408b7c
neal <neal0420yang@gmail.com>
2024-06-16 20:18:57 +0800

    feat(controller)添加controller 中的service & repository的初始化

--- handler/user/index.go
+++ handler/user/index.go
@@ -2,12 +2,22 @@ package user
 
 import (
 	"github.com/gin-gonic/gin"
+	"shopping-cart/repository"
+	"shopping-cart/service"
 )
 
-type Authorization struct{}
+type Authorization struct {
+	service service.UserService
+}
 
 func NewAuthorization(r *gin.RouterGroup) *Authorization {
 	h := &Authorization{}
+
+	userRepo := repository.NewUserRepository()
+
+	userService := service.NewUserService(userRepo)
+
+	h.service = userService
 
 	newRoute(h, r)
 
