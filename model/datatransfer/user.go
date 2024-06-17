21081b3273394827c7edcafbf282e8045771ae2c
neal <neal0420yang@gmail.com>
2024-06-18 00:36:31 +0800

    feat(entity)將service中的struct整理成一個dto提取到entity layer做紀錄

--- model/datatransfer/user.go
+++ model/datatransfer/user.go
@@ -1,7 +1,16 @@
 package datatransfer
 
-type UserPayload struct {
+type LineProfileResponse struct {
 	UserID      string `json:"userId"`
 	DisplayName string `json:"displayName"`
 	Email       string `json:"email"`
+}
+
+type LineTokenResponse struct {
+	AccessToken  string `json:"access_token"`
+	ExpiresIn    int    `json:"expires_in"`
+	TokenType    string `json:"token_type"`
+	Scope        string `json:"scope"`
+	IDToken      string `json:"id_token"`
+	RefreshToken string `json:"refresh_token"`
 }
