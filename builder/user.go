21b7c8ad274159c2de377d51e93a096438eab6d4
neal <neal0420yang@gmail.com>
2024-06-19 23:24:38 +0800

    feat(builder)使用builder pattern將原本在service中的冗余過程提取出來

---
+++ builder/user.go
@@ -0,0 +1,35 @@
+package builder
+
+import "shopping-cart/model/database"
+
+type UserBuilder struct {
+	user *database.User
+}
+
+func NewUserBuilder() *UserBuilder {
+	return &UserBuilder{user: &database.User{}}
+}
+
+func (b *UserBuilder) WithLineID(lineID string) *UserBuilder {
+	b.user.LineID = lineID
+	return b
+}
+
+func (b *UserBuilder) WithDisplayName(displayName string) *UserBuilder {
+	b.user.DisplayName = displayName
+	return b
+}
+
+func (b *UserBuilder) WithEmail(email string) *UserBuilder {
+	b.user.Email = email
+	return b
+}
+
+func (b *UserBuilder) WithLineToken(lineToken string) *UserBuilder {
+	b.user.LineToken = lineToken
+	return b
+}
+
+func (b *UserBuilder) Build() *database.User {
+	return b.user
+}
