eabaac7e1a5abe61b279acdc0c1f0e109830c1dc
neal <neal0420yang@gmail.com>
2024-06-19 23:22:48 +0800

    feat(constant)刪除無用lineprofileURL

--- constant/line.go
+++ constant/line.go
@@ -1,7 +1,6 @@
 package constant
 
 const (
-	LineAuthURL    = "https://access.line.me/oauth2/v2.1/authorize"
-	LineTokenURL   = "https://api.line.me/oauth2/v2.1/token"
-	LineProfileURL = "https://api.line.me/v2/profile"
+	LineAuthURL  = "https://access.line.me/oauth2/v2.1/authorize"
+	LineTokenURL = "https://api.line.me/oauth2/v2.1/token"
 )
