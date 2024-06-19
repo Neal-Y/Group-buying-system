7e55f42ca63b61900a27937ec1d42141d04266f1
neal <neal0420yang@gmail.com>
2024-06-19 23:21:45 +0800

    feat(util)新增parse IDtoken的函數，將userid、displayname、email從IDtoken中解jwt提取出來

--- util/http_call_out.go
+++ util/http_call_out.go
@@ -3,9 +3,12 @@ package util
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/golang-jwt/jwt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"shopping-cart/config"
+	"shopping-cart/model/datatransfer"
 )
 
 func PostForm(url string, data url.Values) ([]byte, error) {
@@ -40,4 +43,30 @@ func ParseJSONResponse(body []byte, v interface{}) error {
 		return fmt.Errorf("failed to unmarshal JSON: %w", err)
 	}
 	return nil
+}
+
+func ParseIDToken(idToken string) (*datatransfer.LineProfileResponse, error) {
+	token, err := jwt.Parse(idToken, func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(config.AppConfig.LineClientSecret), nil
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse id token: %w", err)
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if ok && token.Valid {
+		profileData := &datatransfer.LineProfileResponse{
+			UserID:      claims["sub"].(string),
+			DisplayName: claims["name"].(string),
+			Email:       claims["email"].(string),
+		}
+		return profileData, nil
+	} else {
+		return nil, fmt.Errorf("failed to parse id token")
+	}
 }
