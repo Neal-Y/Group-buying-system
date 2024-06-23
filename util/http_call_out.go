cfc0e202a4b5afbd8df735bdc745b5d716b8f21c
neal <neal0420yang@gmail.com>
2024-06-23 22:29:20 +0500

    feat(util)移除掉Get()和PostForm()兩個函數，將他們整合在httpBuilder當中

--- util/http_call_out.go
+++ util/http_call_out.go
@@ -4,38 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/golang-jwt/jwt"
-	"io/ioutil"
-	"net/http"
-	"net/url"
 	"shopping-cart/config"
 	"shopping-cart/model/datatransfer"
 )
-
-func PostForm(url string, data url.Values) ([]byte, error) {
-	resp, err := http.PostForm(url, data)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-	return ioutil.ReadAll(resp.Body)
-}
-
-func Get(url, accessToken string) ([]byte, error) {
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", accessToken))
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	return ioutil.ReadAll(resp.Body)
-}
 
 func ParseJSONResponse(body []byte, v interface{}) error {
 	err := json.Unmarshal(body, v)
