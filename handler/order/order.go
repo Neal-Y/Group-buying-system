6d08a542313314496ad2bc0c6c696399be589c5e
neal <neal0420yang@gmail.com>
2024-06-25 18:21:41 +0500

    feat(handler)簡化handler流程，將業務邏輯移到service layer

--- handler/order/order.go
+++ handler/order/order.go
@@ -2,115 +2,36 @@ package order
 
 import (
 	"github.com/gin-gonic/gin"
-	"gorm.io/gorm"
 	"net/http"
-	"shopping-cart/infrastructure"
-	"shopping-cart/model/database"
-	"strconv"
-	"time"
+	"shopping-cart/model/datatransfer"
+	"shopping-cart/util"
 )
 
 func (h *Order) CreateOrder(c *gin.Context) {
-	var orderRequest struct {
-		UserID       int                    `json:"user_id" binding:"required"`
-		Note         string                 `json:"note"`
-		OrderDetails []database.OrderDetail `json:"order_details" binding:"required"`
-	}
-
+	var orderRequest datatransfer.OrderRequest
 	err := c.ShouldBindJSON(&orderRequest)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	totalPrice := 0.0
-
-	// 檢查庫存跟有效期以及訂單數量不為零
-	for i, detail := range orderRequest.OrderDetails {
-		if detail.Quantity <= 0 {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "Quantity must be greater than zero"})
-			return
-		}
-
-		var product database.Product
-		err = infrastructure.Db.First(&product, detail.ProductID).Error
-		if err != nil {
-			c.JSON(http.StatusNotFound, gin.H{"error": "Product not found"})
-			return
-		}
-
-		// 检查库存
-		if product.Stock < detail.Quantity {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "Insufficient stock for product " + product.Name})
-			return
-		}
-
-		// 检查产品有效期
-		if time.Now().After(product.ExpirationTime) {
-			c.JSON(http.StatusNotFound, gin.H{"error": "Product " + product.Name + " is expired"})
-			return
-		}
-
-		// order detail 的 price 为 product 的 price
-		orderRequest.OrderDetails[i].Price = product.Price
-
-		// 计算总价格
-		totalPrice += float64(detail.Quantity) * product.Price
-	}
-
-	// 创建订单
-	order := database.Order{
-		UserID:     orderRequest.UserID,
-		TotalPrice: totalPrice,
-		Note:       orderRequest.Note,
-		Status:     "pending",
-		CreatedAt:  time.Now(),
-		UpdatedAt:  time.Now(),
-	}
-
-	// 开始事务
-	tx := infrastructure.Db.Begin()
-
-	if err := tx.Create(&order).Error; err != nil {
-		tx.Rollback()
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create order"})
+	order, err := h.orderService.CreateOrder(&orderRequest)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
-
-	for _, detail := range orderRequest.OrderDetails {
-		detail.OrderID = order.ID
-
-		// 更新库存
-		err = tx.Model(&database.Product{}).Where("id = ?", detail.ProductID).Update("stock", gorm.Expr("stock - ?", detail.Quantity)).Error
-		if err != nil {
-			tx.Rollback()
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update product stock"})
-			return
-		}
-
-		err = tx.Create(&detail).Error
-		if err != nil {
-			tx.Rollback()
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create order detail"})
-			return
-		}
-	}
-
-	tx.Commit()
 
 	c.JSON(http.StatusOK, gin.H{"message": "Order created successfully", "order": order})
 }
 
 func (h *Order) GetOrder(c *gin.Context) {
-	idStr := c.Param("id")
-	id, err := strconv.Atoi(idStr)
+	id, err := util.GetIDFromPath(c, "id")
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid order ID"})
 		return
 	}
 
-	order := &database.Order{}
-	err = order.FindByID(id)
+	order, err := h.orderService.GetOrderByID(id)
 	if err != nil {
 		c.JSON(http.StatusNotFound, gin.H{"error": "Order not found"})
 		return
@@ -120,66 +41,22 @@ func (h *Order) GetOrder(c *gin.Context) {
 }
 
 func (h *Order) UpdateOrder(c *gin.Context) {
-	idStr := c.Param("id")
-	id, err := strconv.Atoi(idStr)
+	id, err := util.GetIDFromPath(c, "id")
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid order ID"})
 		return
 	}
 
-	var updateData database.Order
-	err = c.ShouldBindJSON(&updateData)
+	var orderRequest datatransfer.OrderRequest
+	err = c.ShouldBindJSON(&orderRequest)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	order := &database.Order{}
-	err = order.FindByID(id)
-	if err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": "Order not found"})
-		return
-	}
-
-	totalPrice := 0.0
-
-	for _, detail := range updateData.OrderDetails {
-		var product database.Product
-		err := infrastructure.Db.First(&product, detail.ProductID).Error
-		if err != nil {
-			c.JSON(http.StatusNotFound, gin.H{"error": "Product not found"})
-			return
-		}
-
-		// 检查库存
-		if product.Stock < detail.Quantity {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "Insufficient stock for product " + product.Name})
-			return
-		}
-
-		// 检查产品有效期
-		if time.Now().After(product.ExpirationTime) {
-			c.JSON(http.StatusNotFound, gin.H{"error": "Product " + product.Name + " is expired"})
-			return
-		}
-
-		// 计算总价格
-		totalPrice += float64(detail.Quantity) * product.Price
-	}
-
-	updateData.TotalPrice = totalPrice
-	updateData.ID = id // 确保更新的ID正确
-
-	err = order.Update(&updateData)
+	order, err := h.orderService.UpdateOrder(id, &orderRequest)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-
-	// 重新加载订单信息
-	err = order.FindByID(id)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to reload order"})
 		return
 	}
 
@@ -187,42 +64,23 @@ func (h *Order) UpdateOrder(c *gin.Context) {
 }
 
 func (h *Order) DeleteOrder(c *gin.Context) {
-	id := c.Param("id")
-	var order database.Order
-
-	// 查找订单
-	err := infrastructure.Db.Preload("OrderDetails").First(&order, id).Error
+	id, err := util.GetIDFromPath(c, "id")
 	if err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": "Order not found"})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid order ID"})
 		return
 	}
 
-	// 开始事务
-	tx := infrastructure.Db.Begin()
-
-	// 恢复库存
-	for _, detail := range order.OrderDetails {
-		if err := tx.Model(&database.Product{}).Where("id = ?", detail.ProductID).Update("stock", gorm.Expr("stock + ?", detail.Quantity)).Error; err != nil {
-			tx.Rollback()
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to restore product stock"})
-			return
-		}
-	}
-
-	// 删除订单
-	err = infrastructure.Db.Delete(&order).Error
+	err = h.orderService.DeleteOrder(id)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete order"})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
-
-	tx.Commit()
 
 	c.JSON(http.StatusOK, gin.H{"message": "Order deleted successfully"})
 }
 
 func (h *Order) ListOrders(c *gin.Context) {
-	orders, err := database.FindAllOrders()
+	orders, err := h.orderService.ListAllOrders()
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err})
 		return
