920ecbe76517ccc0f55d8976c5aea9b8ebd333a2
neal <neal0420yang@gmail.com>
2024-06-28 17:36:03 +0800

    feat(user entity)新增for admin操作user repository的dto

--- model/datatransfer/user.go
+++ model/datatransfer/user.go
@@ -14,3 +14,22 @@ type LineTokenResponse struct {
 	IDToken      string `json:"id_token"`
 	RefreshToken string `json:"refresh_token"`
 }
+
+type UserRequest struct {
+	LineID      string `json:"line_id"`
+	DisplayName string `json:"display_name" binding:"required"`
+	Email       string `json:"email"`
+	LineToken   string `json:"line_token"`
+	Phone       string `json:"phone"`
+	IsMember    bool   `json:"is_member"`
+}
+
+type UserResponse struct {
+	ID          int    `json:"id"`
+	LineID      string `json:"line_id"`
+	DisplayName string `json:"display_name"`
+	Email       string `json:"email"`
+	LineToken   string `json:"line_token"`
+	Phone       string `json:"phone"`
+	IsMember    bool   `json:"is_member"`
+}
