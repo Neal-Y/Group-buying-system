30f574cb8c40111b3b77a61f46f9444edd333009
neal <neal0420yang@gmail.com>
2024-06-30 23:43:13 +0800

    feat(service)新增listalladmin的api

--- service/admin.go
+++ service/admin.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"shopping-cart/model/database"
 	"shopping-cart/model/datatransfer/admin"
-	"shopping-cart/model/datatransfer/user"
 	"shopping-cart/repository"
 	"shopping-cart/util"
 
@@ -15,25 +14,18 @@ type AdminService interface {
 	RegisterAdmin(admin *admin.Request) error
 	Login(username, password string) (string, error)
 	GetAdminByID(id int) (*database.Admin, error)
+	GetAllAdmin() ([]database.Admin, error)
 	UpdateAdmin(id int, req *admin.UpdateRequest) (*database.Admin, error)
 	DeleteAdmin(id int) error
-	CreateUser(user *user.Request) error
-	GetUserByID(id int) (*database.User, error)
-	UpdateUser(id int, user *user.Request) error
-	DeleteUser(id int) error
 }
 
 type adminService struct {
 	adminRepo repository.AdminRepository
-	userRepo  repository.UserRepository
-	orderRepo repository.OrderRepository
 }
 
-func NewAdminService(adminRepo repository.AdminRepository, userRepo repository.UserRepository, orderRepo repository.OrderRepository) AdminService {
+func NewAdminService(adminRepo repository.AdminRepository) AdminService {
 	return &adminService{
 		adminRepo: adminRepo,
-		userRepo:  userRepo,
-		orderRepo: orderRepo,
 	}
 }
 
@@ -66,6 +58,10 @@ func (s *adminService) Login(username, password string) (string, error) {
 	}
 
 	return token, nil
+}
+
+func (s *adminService) GetAllAdmin() ([]database.Admin, error) {
+	return s.adminRepo.FindAll()
 }
 
 func (s *adminService) GetAdminByID(id int) (*database.Admin, error) {
@@ -103,57 +99,4 @@ func (s *adminService) DeleteAdmin(id int) error {
 		return err
 	}
 	return s.adminRepo.Delete(admin)
-}
-
-func (s *adminService) CreateUser(req *user.Request) error {
-	user := &database.User{
-		LineID:      "CreatedByAdmin",
-		DisplayName: req.DisplayName,
-		Phone:       req.Phone,
-		IsMember:    req.IsMember,
-	}
-	return s.userRepo.Create(user)
-}
-
-func (s *adminService) GetUserByID(id int) (*database.User, error) {
-	return s.userRepo.FindByID(id)
-}
-
-func (s *adminService) UpdateUser(id int, req *user.Request) error {
-	user, err := s.userRepo.FindByID(id)
-	if err != nil {
-		return err
-	}
-
-	user.LineID = req.LineID
-	user.DisplayName = req.DisplayName
-	user.Email = req.Email
-	user.LineToken = req.LineToken
-	user.Phone = req.Phone
-	user.IsMember = req.IsMember
-
-	return s.userRepo.Update(user)
-}
-
-func (s *adminService) DeleteUser(id int) error {
-	tx := s.userRepo.BeginTransaction()
-
-	pendingOrders, err := s.orderRepo.FindPendingOrdersByUserIDTx(tx, id)
-	if err != nil {
-		tx.Rollback()
-		return err
-	}
-	if len(pendingOrders) > 0 {
-		tx.Rollback()
-		return errors.New("user has pending orders, cannot delete")
-	}
-
-	err = s.userRepo.DeleteTx(tx, id)
-	if err != nil {
-		tx.Rollback()
-		return err
-	}
-
-	tx.Commit()
-	return nil
 }
