f2c2220b002045dfed639ff62eab36c9a775d0b7
neal <neal0420yang@gmail.com>
2024-07-01 19:18:50 +0800

    feat(entity)order dto 增加status

--- model/datatransfer/order.go
+++ model/datatransfer/order.go
@@ -5,5 +5,6 @@ import "shopping-cart/model/database"
 type OrderRequest struct {
 	UserID       int                    `json:"user_id"`
 	Note         string                 `json:"note"`
+	Status       string                 `json:"status"`
 	OrderDetails []database.OrderDetail `json:"order_details"`
 }
