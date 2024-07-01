4000c34b1c816fa960012e9bf58ac5898cbbacd8
neal <neal0420yang@gmail.com>
2024-07-01 11:21:15 +0800

    feat(handler)新增admin 對於 user 的handler函數

--- handler/admin/index.go
+++ handler/admin/index.go
@@ -13,8 +13,10 @@ type Admin struct {
 
 func NewAdminController(r *gin.RouterGroup) *Admin {
 	adminRepo := repository.NewAdminRepository()
+	userRepo := repository.NewUserRepository()
+	orderRepo := repository.NewOrderRepository()
 
-	adminService := service.NewAdminService(adminRepo)
+	adminService := service.NewAdminService(adminRepo, userRepo, orderRepo)
 
 	h := &Admin{
 		adminService: adminService,
@@ -25,6 +27,7 @@ func NewAdminController(r *gin.RouterGroup) *Admin {
 	r.Use(middleware.JWTAuthMiddleware())
 	{
 		adminRoute(h, r)
+		manageUser(h, r)
 	}
 
 	return h
@@ -40,4 +43,11 @@ func adminRoute(h *Admin, r *gin.RouterGroup) {
 	r.GET("/admins", h.ListAdmins)
 	r.PATCH("/admin/:id", h.UpdateAdmin)
 	r.DELETE("/admin/:id", h.DeleteAdmin)
+}
+
+func manageUser(h *Admin, r *gin.RouterGroup) {
+	r.POST("/admin/users", h.CreateUser)
+	r.GET("/admin/users/:id", h.GetUser)
+	r.PATCH("/admin/users/:id", h.UpdateUser)
+	r.DELETE("/admin/users/:id", h.DeleteUser)
 }
