07934e26780433bd02d34704d5d04f5be5d74659
neal <neal0420yang@gmail.com>
2024-07-02 22:01:25 +0800

    feat(entity)新增兩個欄位到update dto

--- model/datatransfer/product/update.go
+++ model/datatransfer/product/update.go
@@ -3,10 +3,12 @@ package product
 import "time"
 
 type Update struct {
-	Name           string    `json:"name"`
-	Picture        string    `json:"picture"`
-	Price          float64   `json:"price" `
-	Stock          int       `json:"stock" `
-	Description    string    `json:"description"`
-	ExpirationTime time.Time `json:"expiration_time" `
+	Name           string     `json:"name"`
+	Picture        string     `json:"picture"`
+	Price          float64    `json:"price" `
+	Stock          int        `json:"stock" `
+	Description    string     `json:"description"`
+	ExpirationTime time.Time  `json:"expiration_time"`
+	IsSoldOut      bool       `json:"is_sold_out,omitempty"`
+	SoldOutAt      *time.Time `json:"sold_out_at,omitempty"`
 }
