36c7d21dc8171fcfb3c3abeaf2082259753c6063
neal <neal0420yang@gmail.com>
2024-07-04 10:15:41 +0800

    feat(handler)將各自API分到各自的file

---
+++ handler/admin/login.go
@@ -0,0 +1,25 @@
+package admin
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"shopping-cart/model/datatransfer/admin"
+)
+
+func (h *Admin) Login(c *gin.Context) {
+	var req admin.Request
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	token, err := h.adminService.Login(req.Username, req.Password)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.SetCookie("Authorization", token, 3600*24*20, "", "", false, true)
+
+	c.JSON(http.StatusOK, gin.H{"message": "login successfully"})
+}
