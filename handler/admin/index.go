676a0cabf72cc15ca9eb7a91d2e517f5f9f86c90
neal <neal0420yang@gmail.com>
2024-07-05 13:40:43 +0800

    feat(admin handler)新增reset API

--- handler/admin/index.go
+++ handler/admin/index.go
@@ -2,6 +2,7 @@ package admin
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 	middleware "shopping-cart/middlerware"
 	"shopping-cart/repository"
 	"shopping-cart/service"
@@ -22,6 +23,8 @@ func NewAdminController(r *gin.RouterGroup) *Admin {
 
 	loginRoute(h, r)
 
+	resetPasswordRoute(h, r)
+
 	r.Use(middleware.JWTAuthMiddleware())
 	{
 		adminRoute(h, r)
@@ -40,4 +43,28 @@ func adminRoute(h *Admin, r *gin.RouterGroup) {
 	r.PATCH("/admin/:id", h.UpdateAdmin)
 	r.DELETE("/admin/:id", h.DeleteAdmin)
 	r.POST("/admin/register", h.Register)
+}
+
+func resetPasswordRoute(h *Admin, r *gin.RouterGroup) {
+	r.POST("/admin/get_email", h.GetAdminEmail)
+	r.POST("/admin/request_password_reset", h.RequestPasswordReset)
+	r.POST("/admin/reset_password", middleware.JWTAuthMiddleware(), h.ResetPassword)
+}
+
+func RegisterHomeRoutes(server *gin.Engine) {
+	server.GET("/admin/login", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "login.html", nil)
+	})
+
+	server.GET("/admin/forgot_password", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "forgot_password.html", nil)
+	})
+
+	server.GET("/admin/reset_password", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "reset_password.html", nil)
+	})
+
+	server.GET("/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
 }
