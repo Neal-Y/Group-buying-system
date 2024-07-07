aad30e7dbacf1e4a09a210acef0751671fb2ee83
neal <neal0420yang@gmail.com>
2024-07-08 00:49:22 +0800

    feat(service)新增user的crud

--- service/user.go
+++ service/user.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"errors"
 	"fmt"
 	"shopping-cart/builder"
 	"shopping-cart/config"
@@ -9,15 +10,22 @@ import (
 	"shopping-cart/model/datatransfer/user"
 	"shopping-cart/repository"
 	"shopping-cart/util"
+	"time"
 )
 
 type UserService interface {
 	SaveOrUpdateUser(user *database.User) error
 	ExchangeTokenAndGetProfile(code string) (*database.User, error)
+	CreateUser(req *user.Request) error
+	GetUserByID(id int) (*database.User, error)
+	GetUsers() ([]database.User, error)
+	UpdateUser(id int, req *user.Update) error
+	DeleteUser(id int) error
 }
 
 type userService struct {
-	repo repository.UserRepository
+	repo  repository.UserRepository
+	order repository.OrderRepository
 }
 
 func NewUserService(repo repository.UserRepository) UserService {
@@ -27,6 +35,8 @@ func NewUserService(repo repository.UserRepository) UserService {
 func (s *userService) SaveOrUpdateUser(user *database.User) error {
 	existingUser, err := s.repo.FindByLineID(user.LineID)
 	if err != nil {
+		user.CreatedAt = time.Now()
+		user.UpdatedAt = time.Now()
 		return s.repo.Create(user)
 	}
 	user.ID = existingUser.ID
@@ -69,4 +79,66 @@ func (s *userService) ExchangeTokenAndGetProfile(code string) (*database.User, e
 		Build()
 
 	return user, nil
+}
+
+func (s *userService) CreateUser(req *user.Request) error {
+	user := builder.NewUserBuilder().
+		WithLineID("CreatedByAdmin").
+		WithDisplayName(req.DisplayName).
+		WithPhone(req.Phone).
+		WithIsMember(req.IsMember).
+		Build()
+
+	return s.repo.Create(user)
+}
+
+func (s *userService) GetUserByID(id int) (*database.User, error) {
+	return s.repo.FindByID(id)
+}
+
+func (s *userService) GetUsers() ([]database.User, error) {
+	return s.repo.FindAll()
+}
+
+func (s *userService) UpdateUser(id int, req *user.Update) error {
+	user, err := s.repo.FindByID(id)
+	if err != nil {
+		return err
+	}
+
+	updatedUser := builder.NewUserBuilder().
+		WithLineID(req.LineID).
+		WithDisplayName(req.DisplayName).
+		WithEmail(req.Email).
+		WithLineToken(req.LineToken).
+		WithPhone(req.Phone).
+		WithIsMember(req.IsMember).
+		Build()
+
+	updatedUser.ID = user.ID
+
+	return s.repo.Update(updatedUser)
+}
+
+func (s *userService) DeleteUser(id int) error {
+	tx := s.repo.BeginTransaction()
+
+	pendingOrders, err := s.order.FindPendingOrdersByUserIDTx(tx, id)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	if len(pendingOrders) > 0 {
+		tx.Rollback()
+		return errors.New("user has pending orders, cannot delete")
+	}
+
+	err = s.repo.DeleteTx(tx, id)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	tx.Commit()
+	return nil
 }
