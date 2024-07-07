965dfb86c1812b7cc9ef346ece76d221f7a79e19
neal <neal0420yang@gmail.com>
2024-07-08 00:50:21 +0800

    feat(handler)handler分成各自檔案

---
+++ handler/user/delete.go
@@ -0,0 +1,23 @@
+package user
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"shopping-cart/util"
+)
+
+func (h *User) DeleteUser(c *gin.Context) {
+	id, err := util.GetIDFromPath(c, "id")
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid user ID"})
+		return
+	}
+
+	err = h.service.DeleteUser(id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "user deleted successfully"})
+}
