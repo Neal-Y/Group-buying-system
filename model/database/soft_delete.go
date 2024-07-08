7e8d279653e5b50f8bf24d8d21fb64580cc54d80
neal <neal0420yang@gmail.com>
2024-07-08 11:43:05 +0800

    feat(user entity)改成直接在user dao中宣告is_deleted & is_member，而不是用組合的方式

--- model/database/soft_delete.go
+++
@@ -1,8 +0,0 @@
-package database
-
-import "time"
-
-type SoftDeleteModel struct {
-	DeletedAt *time.Time `json:"deleted_at" gorm:"default:null"`
-	IsDeleted bool       `json:"is_deleted" gorm:"default:false"`
-}
--- model/database/user.go
+++ model/database/user.go
@@ -10,11 +10,12 @@ type User struct {
 	DisplayName string `gorm:"not null"`
 	Email       string
 	LineToken   string
-	Phone       string    `gorm:"type:varchar(15)"`
-	CreatedAt   time.Time `gorm:"default:CURRENT_TIMESTAMP"`
-	UpdatedAt   time.Time `gorm:"default:CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"`
-	IsMember    bool      `gorm:"default:false"`
-	SoftDeleteModel
+	Phone       string     `gorm:"type:varchar(15)"`
+	CreatedAt   time.Time  `gorm:"default:CURRENT_TIMESTAMP"`
+	UpdatedAt   time.Time  `gorm:"default:CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"`
+	IsMember    bool       `gorm:"default:false"`
+	DeletedAt   *time.Time `json:"deleted_at" gorm:"default:null"`
+	IsDeleted   bool       `json:"is_deleted" gorm:"default:false"`
 }
 
 func (User) TableName() string {
