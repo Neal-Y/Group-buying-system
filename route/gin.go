867c2882fd8f7ce1fcc04e66045d9497f2c55820
neal <neal0420yang@gmail.com>
2024-07-08 11:23:28 +0800

    feat(route)更改順序避免user&admin有套middleware的token驗證擋到product&order的routers

--- route/gin.go
+++ route/gin.go
@@ -21,9 +21,10 @@ func GinRouter() (server *gin.Engine) {
 
 	api := server.Group("/api")
 
-	user.NewAuthorization(api)
 	product.NewProductController(api)
 	order.NewOrderHandler(api)
+
+	user.NewAuthorization(api)
 	admin.NewAdminController(api)
 
 	return server
