50eb65db5fa8386aaf7eb4b850be63dc9fe0de27
neal <neal0420yang@gmail.com>
2024-07-10 22:02:32 +0800

    feat(product service)設定SetID以及刪掉回傳update完成的資訊

--- handler/product/update.go
+++ handler/product/update.go
@@ -18,11 +18,11 @@ func (h *Product) UpdateProduct(c *gin.Context) {
 		return
 	}
 
-	product, err := h.productService.UpdateProduct(id, &productDto)
+	err = h.productService.UpdateProduct(id, &productDto)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"message": "Product updated successfully", "product_repository": product})
+	c.JSON(http.StatusOK, gin.H{"message": "Product updated successfully"})
 }
