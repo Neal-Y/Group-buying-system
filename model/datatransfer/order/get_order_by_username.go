eb34182246e32d599a5c02c1460c4ff40a19f4e4
neal <neal0420yang@gmail.com>
2024-07-19 13:40:59 +0800

    feat(entity)ListHistoryOrdersByUser＆GetOrderByUserDisplayName的dto

---
+++ model/datatransfer/order/get_order_by_username.go
@@ -0,0 +1,5 @@
+package order
+
+type GetOrderByUsername struct {
+	DisplayName string `json:"display_name"`
+}
---
+++ model/datatransfer/order/list_history.go
@@ -0,0 +1,6 @@
+package order
+
+type ListHistory struct {
+	DisplayName string `json:"display_name"`
+	ProductID   int    `json:"product_id"`
+}
