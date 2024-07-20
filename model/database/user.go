b941f4093356ccdcbcecef1312ff4f668db1f36b
neal <neal0420yang@gmail.com>
2024-07-20 20:18:14 +0800

    feat(entity)新增passwordHash到user table dao

--- model/database/user.go
+++ model/database/user.go
@@ -5,15 +5,16 @@ import (
 )
 
 type User struct {
-	ID          int    `gorm:"primary_key"`
-	LineID      string `gorm:"not null"`
-	DisplayName string `gorm:"not null"`
-	Email       string
-	LineToken   string
-	Phone       string     `gorm:"type:varchar(15)"`
-	IsMember    bool       `gorm:"default:false"`
-	DeletedAt   *time.Time `json:"deleted_at" gorm:"default:null"`
-	IsDeleted   bool       `json:"is_deleted" gorm:"default:false"`
+	ID           int    `gorm:"primary_key"`
+	LineID       string `gorm:"not null"`
+	DisplayName  string `gorm:"not null"`
+	Email        string
+	LineToken    string
+	Phone        string     `gorm:"type:varchar(15)"`
+	IsMember     bool       `gorm:"default:false"`
+	DeletedAt    *time.Time `json:"deleted_at" gorm:"default:null"`
+	IsDeleted    bool       `json:"is_deleted" gorm:"default:false"`
+	PasswordHash string     `gorm:"type:varchar(255)"`
 }
 
 func (User) TableName() string {
