8c647829a30b24324de4a8ea3819fbb2d7d83e08
neal <neal0420yang@gmail.com>
2024-07-26 00:20:02 +0800

    feat(service)修改一下通知功能

--- service/notification.go
+++ service/notification.go
@@ -1,17 +1,43 @@
 package service
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/line/line-bot-sdk-go/linebot"
+	"log"
+	"shopping-cart/config"
+)
 
 type NotificationService interface {
 	Notify(productID int, productName string, currentStock int)
 }
 
-type notificationService struct{}
+type notificationService struct {
+	bot    *linebot.Client
+	userID string
+}
 
 func NewNotificationService() NotificationService {
-	return &notificationService{}
+	bot, err := linebot.New(config.AppConfig.LineMsgSecret, config.AppConfig.LineMsgToken)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	userID := config.AppConfig.LineAdminID
+	if userID == "" {
+		log.Fatal("LINE_USER_ID is not set in the environment variables")
+	}
+
+	return &notificationService{
+		bot:    bot,
+		userID: userID,
+	}
 }
 
 func (n *notificationService) Notify(productID int, productName string, currentStock int) {
-	fmt.Printf("Alert: Product %s (ID: %d) stock is below 50%% of the initial stock. Current stock: %d\n", productName, productID, currentStock)
+	message := fmt.Sprintf("提醒: 商品 %s (ID: %d) 庫存已低於50%% 目前剩下: %d個單位", productName, productID, currentStock)
+
+	_, err := n.bot.PushMessage(n.userID, linebot.NewTextMessage(message)).Do()
+	if err != nil {
+		log.Printf("Error sending message: %v", err)
+	}
 }
