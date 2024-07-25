2c0f20c236f442b6949872a57375e7b4a5e055ca
neal <neal0420yang@gmail.com>
2024-07-25 22:14:40 +0800

    feat(util)更改錯誤log msg

--- util/time_validate.go
+++ util/time_validate.go
@@ -1,14 +1,17 @@
 package util
 
-import "time"
+import (
+	"errors"
+	"time"
+)
 
 func ValidateTime(dateStr string) (time.Time, error) {
 	if dateStr == "" {
-		return time.Time{}, nil
+		return time.Time{}, errors.New("date string is empty")
 	}
 	parsedDate, err := time.Parse("2006-01-02", dateStr)
 	if err != nil {
-		return time.Time{}, err
+		return time.Time{}, errors.New("invalid date format")
 	}
 	return parsedDate, nil
 }
