87359514fb22d80f2133c0d9adeb049d8b223ee0
neal <neal0420yang@gmail.com>
2024-07-26 17:17:04 +0800

    feat(entity)新增line_id column到admin dto & dao

--- model/datatransfer/admin/request.go
+++ model/datatransfer/admin/request.go
@@ -4,4 +4,5 @@ type Request struct {
 	Username string `json:"username" binding:"required"`
 	Password string `json:"password" binding:"required"`
 	Email    string `json:"email" binding:"required"`
+	LineID   string `json:"line_id"`
 }
--- model/datatransfer/admin/update_response.go
+++ model/datatransfer/admin/update_response.go
@@ -4,4 +4,5 @@ type UpdateRequest struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
 	Email    string `json:"email"`
+	LineID   string `json:"line_id"`
 }
