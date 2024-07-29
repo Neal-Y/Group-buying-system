63b1c9c747122a2ff8347991669f357c7afc25e8
neal <neal0420yang@gmail.com>
2024-07-29 19:21:07 +0800

    feat(router)改名

--- route/gin.go
+++ route/gin.go
@@ -3,6 +3,7 @@ package route
 import (
 	"github.com/gin-gonic/gin"
 	"shopping-cart/handler/admin"
+	"shopping-cart/handler/admin/render"
 	"shopping-cart/handler/order"
 	"shopping-cart/handler/product"
 	"shopping-cart/handler/user"
@@ -22,7 +23,7 @@ func GinRouter() (server *gin.Engine) {
 	server.Use(gin.Logger())
 	server.LoadHTMLGlob("frontend/*")
 
-	admin.RegisterHomeRoutes(server)
+	render.RegisterHomeRoutes(server)
 
 	api := server.Group("/api")
 
