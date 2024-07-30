b9383e4bfb86d43f3556fe14e42494a532d800b3
neal <neal0420yang@gmail.com>
2024-07-30 14:16:14 +0800

    feat(entity)直接新增一個for admin的dto包含時間

--- model/database/orders.go
+++ model/database/orders.go
@@ -1,5 +1,7 @@
 package database
 
+import "time"
+
 type Order struct {
 	ID           int           `json:"id" gorm:"primary_key;autoIncrement"`
 	UserID       int           `json:"user_id" gorm:"not null"`
@@ -8,6 +10,18 @@ type Order struct {
 	Status       string        `json:"status" gorm:"type:enum('pending', 'completed', 'cancelled');default:'pending'"`
 	User         *User         `json:"user,omitempty" gorm:"foreignKey:UserID"`
 	OrderDetails []OrderDetail `json:"order_details,omitempty" gorm:"foreignKey:OrderID"`
+}
+
+type OrderWitheTime struct {
+	ID           int           `json:"id" gorm:"primary_key;autoIncrement"`
+	UserID       int           `json:"user_id" gorm:"not null"`
+	TotalPrice   float64       `json:"total_price" gorm:"type:decimal(10,2)"`
+	Note         string        `json:"note" gorm:"type:text"`
+	Status       string        `json:"status" gorm:"type:enum('pending', 'completed', 'cancelled');default:'pending'"`
+	User         *User         `json:"user,omitempty" gorm:"foreignKey:UserID"`
+	OrderDetails []OrderDetail `json:"order_details,omitempty" gorm:"foreignKey:OrderID"`
+	CreatedAt    time.Time     `json:"created_at"`
+	UpdatedAt    time.Time     `json:"updated_at"`
 }
 
 func (Order) TableName() string {
